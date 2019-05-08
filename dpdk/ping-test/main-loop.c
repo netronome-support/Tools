eae4422ad591f1c7bc23ffa835d9b7a05b57f78f
Tobias Karlsson <Support@netronome.com>
2019-05-08 11:09:24 -0400

    improving ping-test

--- dpdk/ping-test/main-loop.c
+++ dpdk/ping-test/main-loop.c
@@ -31,43 +31,59 @@ main_loop (void)
     uint64_t max_duration = (uint64_t) (((double) rte_get_tsc_hz()) * g.duration);
     uint64_t passed_tsc = 0;
     uint32_t tsc_last = rte_rdtsc();
-    uint64_t collect_time_limit;
+    uint64_t collect_time_limit = 0;
 
-    /* Start ICMP at a much lower rate */
-    int slow_start = 1;
-    /* At the end allow for the last packets to arrive */
-    int collect = 0;
+    enum { start = 0, execute, collect } phase = start;
 
     while (!g.force_quit) {
 
         uint32_t tsc_now = rte_rdtsc();
         uint32_t diff = tsc_now - tsc_last;
-        passed_tsc += diff;
         tsc_last = tsc_now;
 
-        if (likely(max_duration > 0) && unlikely(passed_tsc > max_duration)) {
-            if (collect == 0) {
+        if (likely(phase == execute)) {
+            passed_tsc += diff;
+            if (likely(g.duration != 0.0) && unlikely(passed_tsc > max_duration)) {
                 collect_time_limit = passed_tsc + 2 * rte_get_tsc_hz();
+                phase = collect;
             }
-            collect = 1;
+        } else
+        if (likely(phase == collect)) {
+            passed_tsc += diff;
+            if (unlikely(passed_tsc > collect_time_limit)) {
+                break;
+            }
+            if (unlikely(icmp_stats.rx_seq_num == icmp_stats.tx_seq_num)) {
+                uint64_t delay = passed_tsc + rte_get_tsc_hz() / 4;
+                collect_time_limit = min(collect_time_limit, delay);
+            }
+        } else {
+            /* phase == start */
+            if ((icmp_stats.rx_pkt_cnt >= 5) ||
+                (icmp_stats.tx_pkt_cnt >= 10)) {
+                if (icmp_stats.rx_pkt_cnt < 5) {
+                    /* Poor or no response - Terminate test */
+                    char ts[32];
+                    fprintf(stderr,
+                        "ERROR: %s response from target (%s)\n",
+                        (icmp_stats.rx_pkt_cnt == 0) ? "no" : "poor",
+                        ipaddr_str(ts, g.r_ipv4_addr));
+                    break;
+                }
+                /* Set the rate to specified rate */
+                rate_limit_setup(&rl, g.rate, 8.0);
+                phase = execute;
+                g.measure_latency = true;
+            }
         }
 
         int i, credits = rate_limit_get_credit(&rl);
         if (unlikely(credits > 0)) {
 
-            if (unlikely(collect)) {
-                credits = 0;
-                if (unlikely(passed_tsc > collect_time_limit)) {
-                    break;
-                }
-                if (unlikely(icmp_stats.rx_seq_num == icmp_stats.tx_seq_num)) {
-                    uint64_t delay = passed_tsc + rte_get_tsc_hz() / 4;
-                    collect_time_limit = min(collect_time_limit, delay);
-                }
-            } else {
+            if (likely(phase != collect)) {
                 if (unlikely(g.count > 0) && (icmp_stats.tx_pkt_cnt + credits > g.count)) {
                     collect_time_limit = passed_tsc + 2 * rte_get_tsc_hz();
-                    collect = 1;
+                    phase = collect;
                     credits = min(0, g.count - icmp_stats.tx_pkt_cnt);
                 }
 
@@ -75,25 +91,6 @@ main_loop (void)
 
                 for (i = 0 ; i < credits ; i++) {
                     icmp_gen_request(g.r_ipv4_addr);
-                }
-            }
-
-            if (unlikely(slow_start)) {
-                if ((icmp_stats.rx_pkt_cnt >= 5) ||
-                    (icmp_stats.tx_pkt_cnt >= 10)) {
-                    if (icmp_stats.rx_pkt_cnt < 5) {
-                        /* Poor or no response - Terminate test */
-                        char ts[32];
-                        fprintf(stderr,
-                            "ERROR: %s response from target (%s)\n",
-                            (icmp_stats.rx_pkt_cnt == 0) ? "no" : "poor",
-                            ipaddr_str(ts, g.r_ipv4_addr));
-                        break;
-                    }
-                    /* Set the rate to specified rate */
-                    rate_limit_setup(&rl, g.rate, 8.0);
-                    slow_start = 0;
-                    g.measure_latency = true;
                 }
             }
         }
